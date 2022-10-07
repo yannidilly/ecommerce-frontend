edbcf81f22b1d35abe352223d4fef7695ef3fe3d
Igor Carvalho <igorr.ackerman@gmail.com>
2022-10-07 17:54:54 -0300

    Lista categorias de produtos (Requisito 4)

--- src/App.js
+++ src/App.js
@@ -2,13 +2,43 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import './App.css';
+import { getCategories } from './services/api';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Route exact path="/" component={ Home } />
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+  }
+
+  async componentDidMount() {
+    const cat = await getCategories();
+    this.setState({ categories: cat });
+  }
+
+  render() {
+    const { categories } = this.state;
+    return (
+      <>
+        <div>
+          { categories.map((obj) => (
+            <button
+              type="button"
+              key={ obj.id }
+              data-testid="category"
+            >
+              { obj.name }
+            </button>
+          )) }
+        </div>
+        <BrowserRouter>
+          <Route exact path="/" component={ Home } />
+        </BrowserRouter>
+      </>
+
+    );
+  }
 }
 
 export default App;
