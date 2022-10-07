935cb73cfb21c85e0bdf084c4e31804bdd9f8e88
Yanni <yannidilly.prog@gmail.com>
2022-10-07 17:02:01 -0300

    requisito 2, implementa página Home

---
+++ src/pages/Home.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class Home extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+    };
+  }
+
+  onInputChange = (event) => {
+    this.setState({
+      searchText: event.target.value,
+    });
+  };
+
+  render() {
+    const { searchText } = this.state;
+    return (
+      <section>
+        <h1>HOME</h1>
+        <input
+          type="text"
+          name="search"
+          onChange={ this.onInputChange }
+        />
+        {
+          (searchText.length === 0)
+            ? <p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p> : null
+        }
+      </section>
+    );
+  }
+}
+
+export default Home;
