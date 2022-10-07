d08e88283984d92572a53043443003b565ff285c
Igor Carvalho <igorr.ackerman@gmail.com>
2022-10-07 17:01:21 -0300

    Implementa módulo de acesso a API (Requisito 1)

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,14 @@
 export async function getCategories() {
-  // Implemente aqui
+  const req = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const categories = req.json();
+  return categories;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const response = await fetch(url);
+  const jsonData = response.json();
+  return jsonData;
 }
 
 export async function getProductById() {
