8b558404076b5cfeabeb2afbadd336be0221107e
Igor Carvalho <igorr.ackerman@gmail.com>
2022-10-07 20:20:17 -0300

    Lista os produtos buscados (Requisito 5)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class Home extends React.Component {
   constructor() {
     super();
-
     this.state = {
       searchText: '',
+      items: [],
+      nothing: '',
     };
   }
+
+  onBtnClick = async () => {
+    const { searchText } = this.state;
+    const searchedItems = await getProductsFromCategoryAndQuery('', searchText);
+    if (searchedItems.results.length === 0) {
+      return this.setState({ searchText: '', nothing: 'Nenhum produto foi encontrado' });
+    }
+    this.setState({ searchText: '', items: searchedItems });
+  };
 
   onInputChange = (event) => {
     this.setState({
@@ -19,17 +30,27 @@ class Home extends React.Component {
 
   render() {
     const { history } = this.props;
-    const { searchText } = this.state;
+    const { searchText, items, nothing } = this.state;
+    const { results } = items;
     return (
       <section>
         <Header history={ history } />
         <input
+          data-testid="query-input"
           type="text"
           name="search"
           onChange={ this.onInputChange }
+          value={ searchText }
         />
+        <button
+          data-testid="query-button"
+          type="button"
+          onClick={ this.onBtnClick }
+        >
+          Buscar
+        </button>
         {
-          (searchText.length === 0)
+          (items.length === 0)
             ? (
               <p
                 data-testid="home-initial-message"
@@ -37,8 +58,20 @@ class Home extends React.Component {
                 Digite algum termo de pesquisa ou escolha uma categoria.
 
               </p>
-            ) : null
+            )
+            : (
+              <div className="itemDiv">
+                {results.map((obj) => (
+                  <div data-testid="product" className="item" key={ obj.id }>
+                    <img src={ obj.thumbnail } alt={ obj.title } />
+                    <h3>{ obj.title }</h3>
+                    <h2>{ Number(obj.price.toFixed(2)) }</h2>
+                  </div>
+                ))}
+              </div>
+            )
         }
+        { nothing !== '' && <h3>{ nothing }</h3> }
       </section>
     );
   }
