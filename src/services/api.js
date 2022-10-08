d54e1125c4c8795424aec4a1f445473126fcdde1
Igor Carvalho <igorr.ackerman@gmail.com>
2022-10-08 13:31:07 -0300

    Mostra os produtos de uma categoria

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,8 @@ export async function getCategories() {
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  let url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  if (!query) url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
   const response = await fetch(url);
   const jsonData = response.json();
   return jsonData;
