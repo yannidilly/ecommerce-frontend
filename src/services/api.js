49db3dfc342f693a8ebd2f8c9c9a4dc5cca002b0
Igor Carvalho <igorr.ackerman@gmail.com>
2022-10-08 18:11:15 -0300

    Redireciona para tela sobre informações do produto (Requisito 7)

--- src/services/api.js
+++ src/services/api.js
@@ -12,7 +12,9 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   return jsonData;
 }
 
-export async function getProductById() {
-  // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
-  // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
+export async function getProductById(id) {
+  const api = `https://api.mercadolibre.com/items/${id}`;
+  const apiResp = await fetch(api);
+  const respJson = apiResp.json();
+  return respJson;
 }
